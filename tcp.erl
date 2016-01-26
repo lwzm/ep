47092125e35e79a29c2ad17d1bfb8a4f8f706777
felloak <felloak@gmail.com>
2016-01-26 20:42:06 +0800

    connected

--- tcp.erl
+++ tcp.erl
@@ -1,44 +1,93 @@
 -module(tcp).
 
--export([run/0]).
--export([continue/1]).
--export([do_loop/1]).
+-export([run/0, tcp_accept/1, udp_read/1, udp_write/1]).
+-export([tcp_accept_loop/2]).
 -define(PORT, 1024).
 
 run() ->
-    spawn(fun() -> server(1111) end).
+    Port = 1111,
+    {ok, TCPSocket} = gen_tcp:listen(Port, [binary, {active, false}]),
+    {ok, UDPSocket} = gen_udp:open(Port, [binary, {active, true}]),
+    TCPAcceptPid = spawn(?MODULE, tcp_accept, [TCPSocket]),
+    UDPReaderPid = spawn(?MODULE, udp_read, [UDPSocket]),
+    UDPWriterPid = spawn(?MODULE, udp_write, [UDPSocket]),
+    register(my_tcp, TCPAcceptPid),
+    register(my_udp_reader, UDPReaderPid),
+    register(my_udp_writer, UDPWriterPid),
+    gen_tcp:controlling_process(TCPSocket, TCPAcceptPid),
+    gen_udp:controlling_process(UDPSocket, UDPReaderPid),
+    running.
 
-server(Port) ->
-    {ok, LSock} = gen_tcp:listen(Port, [{active, false}]),
-    io:format("tcp listen: ~p on ~p ~n", [LSock, Port]),
-    accept(LSock).
 
-accept(LSock) ->
-    {ok, ASock} = gen_tcp:accept(LSock),
-    io:format("tcp accept: ~p -> ~p ~n", [LSock, ASock]),
-    Pid = spawn(?MODULE, do_loop, [ASock]),
-    gen_tcp:controlling_process(ASock, Pid),
-    inet:setopts(ASock, [{active, 1}]),
-    accept(LSock).
+tcp_accept(LSocket) ->
+    tcp_accept(LSocket, 0).
 
-do_loop(ASock) ->
+tcp_accept(LSocket, ID) ->
+    {ok, ASocket} = gen_tcp:accept(LSocket),
+    io:format("tcp accept: ~p -> ~p ~n", [LSocket, ASocket]),
+    Pid = spawn(?MODULE, tcp_accept_loop, [ASocket, ID]),
+    my_udp_reader ! {client_login, ID, Pid},%login
+    gen_tcp:controlling_process(ASocket, Pid),
+    inet:setopts(ASocket, [{packet, line}, {active, 3}]),
+    tcp_accept(LSocket, ID + 1).
+
+
+tcp_accept_loop(Socket, ID) ->
     receive
         {tcp, Socket, Data} ->
-            io:format("socket ~p recv: ~p ~n", [Socket, Data]);
-        {tcp_closed, Socket} ->
-            io:format("socket ~p closed ~n", [Socket]);
+            my_udp_writer ! {to_py, ID, Data},
+            %io:format("socket ~p recv: ~p ~n", [Socket, Data]),
+            tcp_recv;
+        {reply_client, Data} ->
+            gen_tcp:send(Socket, Data),
+            %io:format("socket ~p recv: ~p ~n", [Socket, Data]),
+            tcp_reply;
         {tcp_passive, Socket} ->
-            io:format("socket ~p is passive, please call continue/1 ~p ~n", [Socket, self()]);
-        {release_passive, N} ->
-            inet:setopts(ASock, [{active, N}]);
+            inet:setopts(Socket, [{active, 3}]),
+            %io:format("socket ~p is passive, please call continue/1 ~p ~n", [Socket, self()]);
+            passive;
+        {tcp_closed, Socket} ->
+            %io:format("socket ~p closed ~n", [Socket]),
+            my_udp_reader ! {client_logout, ID},
+            tcp_close;
         Err ->
-            io:format("socket may error: ~p ~n", [Err])
+            io:format("socket error: ~p ~n", [Err])
     end,
-    do_loop(ASock).
+    tcp_accept_loop(Socket, ID).
 
-continue(Pid, N) ->
-    Pid ! {release_passive, N},
-    ok.
 
-continue(Pid) ->
-    continue(Pid, 3).
+udp_read(Socket) ->
+    receive
+        {udp, Socket, Host, Port, Bin} ->
+            %io:format("server received:~p from ~p:~p~n", [Bin, Host, Port]), 
+            <<ID, Data/binary>> = Bin,
+            send_to_tcp_client(get(ID), Data),
+            udp;
+        {client_login, ID, Pid} ->
+            put(ID, Pid),
+            client_login_and_record_client;
+        {client_logout, ID} ->
+            erase(ID),
+            io:format("logout and ~p ~n", [get()]), 
+            client_logout_and_erase_client;
+        Other ->
+            io:format("server received: ~p ~n", [Other])
+    end,
+    udp_read(Socket).
+
+
+send_to_tcp_client(undefined, _Data) ->
+    do_nothing;
+send_to_tcp_client(Pid, Data) when is_pid(Pid) ->
+    Pid ! {reply_client, Data}.
+
+udp_write(Socket) ->
+    receive
+        {to_py, ID, Data} ->
+            %io:format("~p ~p ~p ~n", [ID, Data, <<ID, Data/binary>>]),
+            gen_udp:send(Socket, "sv", 1514, <<ID, Data/binary>>),
+            to_py;
+        Other ->
+            io:format("server received: ~p ~n", [Other])
+    end,
+    udp_write(Socket).
---
+++ udp.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import datetime
+import logging
+import multiprocessing
+import os
+import signal
+import sys
+
+
+num_of_children = 0 if len(sys.argv) < 2 else int(sys.argv[1])
+assert num_of_children >= 0, num_of_children
+children = []
+
+
+def log_to_stderr(*args):
+    print(datetime.datetime.now(), *args, file=sys.stderr)
+
+
+def _term(signal_number=None, stack_frame=None):
+    log_to_stderr("SIGTERM", len(children), children)
+    dead_children = []
+    for seq, pid in children:
+        os.kill(pid, signal.SIGTERM)
+        dead_children.append(os.wait())
+    log_to_stderr("waited", len(dead_children), dead_children)
+    sys.exit()
+
+
+def bind_udp_socket(port, host="0.0.0.0"):
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    return s
+
+
+def loop(port=1514):
+    s = bind_udp_socket(port)
+
+    num_of_locks = (num_of_children + 1) ** 2
+    assert num_of_locks == pow(num_of_children + 1, 2), num_of_locks
+    _Lock = multiprocessing.Lock or multiprocessing.Semaphore
+    locks = [_Lock() for _ in range(num_of_locks)]
+
+    for i in range(num_of_children):
+        seq = i + 1
+        pid = os.fork()
+        if pid == 0:  # child
+            break
+        children.append((seq, pid))
+    else:  # only father run these
+        seq = 0
+        signal.signal(signal.SIGTERM, _term)
+        with open(".pid", "w") as f:
+            f.write(str(os.getpid()))
+        log_to_stderr("father and children:", os.getpid(), children)
+
+    pid = os.getpid()
+    msg = None
+
+    while True:
+        try:
+            msg, addr = s.recvfrom(65536)
+
+            lock_id = sum(msg[:12]) % num_of_locks
+
+            with locks[lock_id]:
+                print(seq, pid, msg)
+                #msg = str(, [addr, pid]).encode() + msg
+                s.sendto(msg, addr)
+
+        except Exception as e:
+            logging.exception(
+                "{} {} {}".format(datetime.datetime.now(), e, msg))
+
+
+if __name__ == "__main__":
+    loop()
